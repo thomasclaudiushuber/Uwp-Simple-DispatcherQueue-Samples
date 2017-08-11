1b113868486e0e6e1e8d3e94c843bf0a2bba6b13
Thomas Claudius Huber <thomas@thomasclaudiushuber.com>
2017-08-11 10:00:21 +0000

    Add DispatcherQueue on UI Thread sample

---
+++ DispatcherQueueToUpdateUIThread/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace DispatcherQueueToUpdateUIThread
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            await StartLongRunningOperationAsync();
+        }
+
+        private async Task StartLongRunningOperationAsync()
+        {
+            var dispatcherQueue = DispatcherQueue.GetForCurrentThread();
+            await Task.Run(async () =>
+             {
+                 dispatcherQueue.TryEnqueue(() => progressBar.Value = 10);
+
+                 // Task Delay is used to simulate some work on this Thread
+                 await Task.Delay(1000);
+
+                 // Just accessing the progressBar does not work, as we are on a separate thread.
+                 // But try it: Comment out this line, and you'll get an Exception
+                 // progressBar.Value = 50;
+
+                 dispatcherQueue.TryEnqueue(() => progressBar.Value = 50);
+                 
+
+                 await Task.Delay(1000);
+                 dispatcherQueue.TryEnqueue(() => progressBar.Value = 100);
+             });
+        }
+    }
+}
